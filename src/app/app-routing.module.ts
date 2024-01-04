aa418bc17ed82c93e2687ba119eac0e6caf4a276
ganaambhore <ganeshambhore555@gmail.com>
2024-01-04 12:14:39 +0530

    kkj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './templates/home/home.component';
+import { AboutComponent } from './templates/about/about.component';
+import { HeaderComponent } from './templates/header/header.component';
+import { ContactComponent } from './templates/contact/contact.component';
+import { LoginComponent } from './includes/login/login.component';
+import { AppLayoutComponent } from './includes/app-layout/app-layout.component';
+import { EmiCalculatorComponent } from './templates/emi-calculator/emi-calculator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home',component:HomeComponent,children:[
+    {path:'',redirectTo:'about',pathMatch:'full'},
+    {path:'about',component:AboutComponent},
+    {path:'contact',component:ContactComponent},
+    {path:'login',component:LoginComponent},
+    {path:'emiCalc',component:EmiCalculatorComponent}
+
+  ]},
+  {
+    path:'apnafinance',component:AppLayoutComponent, children:[
+
+      {path:'admin', loadChildren:()=>import('./modules/admin/admin.module').then(m=>m.AdminModule)},
+      {path:'re' ,loadChildren:()=>import('./modules/re/re.module').then(m=>m.ReModule)}
+
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './templates/home/home.component';
+import { HeaderComponent } from './templates/header/header.component';
+import { AboutComponent } from './templates/about/about.component';
+import { ContactComponent } from './templates/contact/contact.component';
+import { LoginComponent } from './includes/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppLayoutComponent } from './includes/app-layout/app-layout.component';
+import { EmiCalculatorComponent } from './templates/emi-calculator/emi-calculator.component';
+import { AppHeaderComponent } from './includes/app-header/app-header.component';
+import { AppMenuComponent } from './includes/app-menu/app-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    AboutComponent,
+    ContactComponent,
+    LoginComponent,
+    AppLayoutComponent,
+    EmiCalculatorComponent,
+    AppHeaderComponent,
+    AppMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
